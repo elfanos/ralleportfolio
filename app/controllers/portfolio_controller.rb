7092e2068d6aa8bff257a327bf49a2bcdbe95160
elfanos <elfanos@gmail.com>
2016-11-11 10:05:39 +0100

    initial commit

---
+++ app/controllers/portfolio_controller.rb
@@ -0,0 +1,17 @@
+class PortfolioController < ApplicationController
+
+  def index
+
+    # if params[:genre]
+    #   respond_to do |format|
+    #     # format.html
+    #     # format.json
+    #     format.js
+    #   end
+    # end
+
+  end
+  def about_me
+
+  end
+end
